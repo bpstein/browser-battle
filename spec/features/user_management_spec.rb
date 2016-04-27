e989762dc7708b7696446d548c9cf0b85accaf57
Ben Stein <bumstain@gmail.com>
2016-04-27 20:57:33 +1000

    Added HP features

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -12,4 +12,14 @@ describe "Player management", :type => :feature do
     end 
   end
 
+  feature "See player HP" do 
+    scenario "Player 1 can see Player 2's HP" do 
+      visit('/')
+      fill_in :name_1, :with => 'name_1'
+      fill_in :name_2, :with => 'name_2'
+      click_button 'Submit'
+      expect(page).to have_content 'name_2 HP: 100'
+    end
+  end
+
 end
