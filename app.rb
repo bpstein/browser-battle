c9c732a88eae5753bca5a180181b8ac775d0c96c
letianw <tmac4ever0824@gmail.com>
2016-04-29 20:51:11 +1000

    #30 using class instance variable completed

--- app.rb
+++ app.rb
@@ -14,18 +14,18 @@ class Battle < Sinatra::Base
 	post '/names' do
 	  player_1 = Player.new(params[:player_1_name])
 	  player_2 = Player.new(params[:player_2_name])
-	  $game = Game.new(player_1, player_2)
+	  Game.new_game(player_1, player_2)
 	  redirect '/play'
 	end
 
 	get '/play' do
-	  @game = $game
+	  @game = Game.current_game
 	  erb :play
 	end
 
 	post '/attack' do
-	  $game.attack($game.opponent_of($game.current_turn))
-	  if $game.game_over?
+	  Game.current_game.attack(Game.current_game.opponent_of(Game.current_game.current_turn))
+	  if Game.current_game.game_over?
 	  	redirect '/game_over'
 	  else
 	  	redirect '/attack'
@@ -33,17 +33,17 @@ class Battle < Sinatra::Base
 	end
 
 	get '/attack' do
-		@game = $game
+		@game = Game.current_game
 		erb :attack
 	end
 
 	post '/switch-turns' do
-		$game.switch_turn
+		Game.current_game.switch_turn
 		redirect('/play')
 	end
 
 	get '/game_over' do
-		@game = $game
+		@game = Game.current_game
 		erb :game_over
 	end
 
--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,18 @@ class Game
 
 attr_reader :player_1, :player_2, :current_turn, :attacked
 
+  class << self
+
+    def new_game(player_1, player_2)
+      @current_game = Game.new(player_1, player_2)
+    end
+
+    def current_game
+      @current_game
+    end
+
+  end
+
   def initialize(player_1, player_2)
     @players = [player_1, player_2]
     @current_turn = player_1
